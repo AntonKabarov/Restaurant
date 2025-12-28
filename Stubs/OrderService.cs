29f17f3ce952df741507bd9ce37e8df394bb8860
AntonKabarov <kabarov.anton@mail.ru>
2025-12-29 00:10:03 +0300

    Add files via upload

---
+++ Stubs/OrderService.cs
@@ -0,0 +1,27 @@
+﻿using RestaurantLibrary.Dto;
+using RestaurantLibrary.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantLibrary.Stubs
+{
+    public interface IOrderService
+    {
+        Task<OrderResult> CreateOrderAsync(OrderData order);
+    }
+   
+    public class OrderService : IOrderService
+    {
+        public Task<OrderResult> CreateOrderAsync(OrderData order)
+        {
+            return Task.FromResult(new OrderResult(
+                $"STUB_{Guid.NewGuid()}",
+                order.Items.Sum(i => i.Quantity * i.Price),
+                "STUB_STATUS",
+                 DateTime.Now));
+        }
+    }
+}
