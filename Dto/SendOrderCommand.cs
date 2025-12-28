03f8a2721e6e7984635a51fe7bca22e02cdfa95d
AntonKabarov <kabarov.anton@mail.ru>
2025-12-29 00:09:24 +0300

    Add files via upload

---
+++ Dto/SendOrderCommand.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantLibrary.Dto
+{
+    // Основная команда SendOrder
+    public record SendOrderCommand : IRequest<OrderResponse>
+    {
+        public string OrderId { get; init; }
+        public List<OrderMenuItem> MenuItems { get; init; }
+    }
+
+    // Позиция в заказе
+    public record OrderMenuItem
+    {
+        public string Id { get; init; }
+        public decimal Quantity { get; init; }
+    }
+
+}
